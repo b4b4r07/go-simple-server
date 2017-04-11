e6fa969151bf675925be2c5a7a2c64c3789cf696
b4b4r07 <b4b4r07@gmail.com>
2017-04-12 00:35:24 +0900

    improve port

--- port/port.go
+++ port/port.go
@@ -3,6 +3,7 @@ package port
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"strconv"
 )
@@ -13,6 +14,7 @@ func Get(ports ...interface{}) int {
 		l   *net.TCPListener
 		n   int
 		err error
+		ok  bool
 	)
 	for _, port := range ports {
 		switch port.(type) {
@@ -26,20 +28,27 @@ func Get(ports ...interface{}) int {
 		default:
 			continue
 		}
+		ok = true
+		if !Available(n) {
+			ok = false
+			continue
+		}
 
 		host := fmt.Sprintf("localhost:%d", n)
 		addr, err := net.ResolveTCPAddr("tcp", host)
 		if err != nil {
+			log.Printf("skip %d ...", n)
 			continue
 		}
 
 		l, err = net.ListenTCP("tcp", addr)
 		if err != nil {
+			log.Printf("skip %d ...", n)
 			continue
 		}
 		defer l.Close()
 	}
-	if len(ports) == 0 {
+	if len(ports) == 0 || !ok {
 		host := fmt.Sprintf("localhost:%d", 0)
 		addr, err := net.ResolveTCPAddr("tcp", host)
 		if err != nil {
@@ -53,4 +62,13 @@ func Get(ports ...interface{}) int {
 		defer l.Close()
 	}
 	return l.Addr().(*net.TCPAddr).Port
+}
+
+func Available(port int) bool {
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return false
+	}
+	defer l.Close()
+	return true
 }
